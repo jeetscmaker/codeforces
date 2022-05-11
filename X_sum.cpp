53d2cc2333a2606ce7807b344161a8049acf4ae9
Jitendra <jeetscmaker@gmail.com>
2022-05-11 12:13:27 +0530

    contest: Codeforces Round #790 (Div. 4), problem: (D) X-Sum

---
+++ X_sum.cpp
@@ -0,0 +1,41 @@
+// contest: Codeforces Round #790 (Div. 4), problem: (D) X-Sum
+
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> dx = {1, 1, -1, -1};
+vector<int> dy = {1, -1, 1, -1};
+
+int main(){
+  int t;
+  cin >> t;
+  while(t--) solve();
+  return 0;
+}
+
+void solve(){
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> a(n, vector<int>(m));
+    for (int j = 0; j < n; j++){
+      for (int k = 0; k < m; k++){
+        cin >> a[j][k];
+      }
+    }
+    int ans = 0;
+    for (int j = 0; j < n; j++){
+      for (int k = 0; k < m; k++){
+        int sum = a[j][k];
+        for (int l = 0; l < 4; l++){
+          int x = j + dx[l];
+          int y = k + dy[l];
+          while (0 <= x && x < n && 0 <= y && y < m){
+            sum += a[x][y];
+            x += dx[l];
+            y += dy[l];
+          }
+        }
+        ans = max(ans, sum);
+      }
+    }
+    cout << ans << endl;
+}
